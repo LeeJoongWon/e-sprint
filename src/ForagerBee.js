38af47862957c831be992706d7a3e0b8f94c7a66
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-09-22 14:59:55 +0900

    과제완료

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,7 +1,17 @@
 const Bee = require('./Bee');
 
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+    // TODO..
+    constructor() {
+        super();
+        this.age = 10;
+        this.job = `find pollen`;
+        this.canFly = true;
+        this.treasureChest = [];
+    }
+    forage(data) {
+        this.treasureChest.push(data);
+    }
 }
 
 module.exports = ForagerBee;
