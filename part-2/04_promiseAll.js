a24dfc084e4f2ce726a3cc6262a8291aec58b28b
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-09-27 11:52:22 +0900

    파트3과제 완료

--- part-2/04_promiseAll.js
+++ part-2/04_promiseAll.js
@@ -5,19 +5,19 @@ const user1Path = path.join(__dirname, 'files/user1.json');
 const user2Path = path.join(__dirname, 'files/user2.json');
 
 const readAllUsers = () => {
-  // TODO: Promise.all을 이용해 작성합니다
-  return Promise.all([
-    getDataFromFilePromise(user1Path),
-    getDataFromFilePromise(user2Path)
-  ]).then((userPath) => {
-    return  userPath.map((sumPath) => {
-      return JSON.parse(sumPath)
-    })  
-  })
-}
+    // TODO: Promise.all을 이용해 작성합니다
+    return Promise.all([
+        getDataFromFilePromise(user1Path),
+        getDataFromFilePromise(user2Path),
+    ]).then((userPath) => {
+        return userPath.map((sumPath) => {
+            return JSON.parse(sumPath);
+        });
+    });
+};
 
 // readAllUsers()
 
 module.exports = {
-  readAllUsers
-}
+    readAllUsers,
+};
