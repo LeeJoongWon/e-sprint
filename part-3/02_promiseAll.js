fe9f0427bf33b4b5c19a19daf3018b7e972f5959
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-09-27 12:08:46 +0900

    파트3과제 완료

--- part-3/02_promiseAll.js
+++ part-3/02_promiseAll.js
@@ -1,9 +1,18 @@
 function getNewsAndWeatherAll() {
-  // TODO: Promise.all을 이용해 작성합니다
+    const newsURL = 'http://localhost:4999/data/latestNews';
+    const weatherURL = 'http://localhost:4999/data/weather';
+    let obj = {};
+    let news = fetch(newsURL).then((response) => response.json());
+    let weather = fetch(weatherURL).then((response) => response.json());
+    return Promise.all([news, weather]).then((data) => {
+        obj.news = data[0].data;
+        obj.weather = data[1];
+        return obj;
+    });
 }
 
 if (typeof window === 'undefined') {
-  module.exports = {
-    getNewsAndWeatherAll
-  }
+    module.exports = {
+        getNewsAndWeatherAll,
+    };
 }
